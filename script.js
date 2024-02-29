117975e825df60167a1a8155aeb8f8593df3d39d
Ilya30432 <IlyaS30432@gmail.com>
2024-02-29 23:07:28 +0100

    Adding a number check

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const operator = prompt('Введите математические операторы "+,-,/,*"');
 const num1 = +prompt(`Введите первое число`);
-const num2 = +prompt(`Введите второе число`);
-
-switch(operator){
-    case "-":
-    alert(num1 - num2);
-    break;
-    case "+":
-    alert(num1 + num2);
-    break;
-    case "*":
-    alert(num1 * num2);
-    break;
-    case "/":
-        if(num2 == 0){
-            alert("Ошибка");
+if(!isNaN(num1)){
+    const num2 = +prompt(`Введите второе число`);
+    if(!isNaN(num2)){
+        switch(operator){
+            case "-":
+            alert(num1 - num2);
+            break;
+            case "+":
+            alert(num1 + num2);
+            break;
+            case "*":
+            alert(num1 * num2);
+            break;
+            case "/":
+                if(num2 === 0){
+                    alert("Ошибка");
+                };
+            alert(num1 / num2);
+            break;
+            default: 
+            alert("Такого оператора нету");
         };
-    alert(num1 / num2);
-    break;
-    default: 
-    alert("Такого оператора нету");
+    } else{
+        alert("Вы ввели не число!!!");
+    };
+} else{
+    alert("Вы ввели не число!!!");
 };
+
